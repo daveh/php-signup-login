9401c0eb9ce42841f06ff74cb8ed30641b7653d2
Progen Sama <76449104+ProGen18@users.noreply.github.com>
2022-10-15 15:53:44 +0200

    Update database.php

--- database.php
+++ database.php
@@ -5,10 +5,7 @@ $dbname = "login_db";
 $username = "root";
 $password = "";
 
-$mysqli = new mysqli(hostname: $host,
-                     username: $username,
-                     password: $password,
-                     database: $dbname);
+$mysqli = new mysqli($host_name, $user_name, $password, $database);
                      
 if ($mysqli->connect_errno) {
     die("Connection error: " . $mysqli->connect_error);
